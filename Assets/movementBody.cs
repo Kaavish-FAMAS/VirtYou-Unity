cc006a3f343eaa16650b129700a862c9ad445686
Fahad Shaikh <fs05452@st.habib.edu.pk>
2022-12-05 18:02:01 +0500

    [FEAT] Fix resolution and collision detection.

--- Assets/movementBody.cs
+++ Assets/movementBody.cs
@@ -8,6 +8,7 @@ public class movementBody : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float movementSpeed = 6f;
     [SerializeField] float jumpForce = 5f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,10 +16,14 @@ public class movementBody : MonoBehaviour
 
     void Update()
     {
-
         float horizontalInput = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontalInput.ToString());
         float verticalInput = Input.GetAxis("Vertical");
+        //Debug.Log(verticalInput.ToString());
+
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        rb.velocity.Normalize();
+
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
