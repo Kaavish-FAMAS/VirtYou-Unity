a9739d203e6cdadcdcfd5825750067cfca20b45b
ar230500 <60130224+ar230500@users.noreply.github.com>
2023-02-24 02:27:36 +0500

    Added New Screens

---
+++ Assets/New UI/Menu/toDT.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toDT : MonoBehaviour
+{
+    public void toDTpage()
+    {
+        SceneManager.LoadScene("LoginModified");
+    }
+}
