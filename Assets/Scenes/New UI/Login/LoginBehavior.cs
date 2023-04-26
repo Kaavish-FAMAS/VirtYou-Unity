f1af06a11b74389f76f840d3640abb9abf0e8146
ar230500 <60130224+ar230500@users.noreply.github.com>
2023-04-26 10:26:30 +0100

    Removed mongoclient code

--- Assets/Scenes/New UI/Login/LoginBehavior.cs
+++ Assets/Scenes/New UI/Login/LoginBehavior.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using MongoDB.Driver;
-using MongoDB.Bson;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
-
+using MongoDB.Bson;
 
 public class LoginBehavior : MonoBehaviour
 {
@@ -18,6 +16,12 @@ public class LoginBehavior : MonoBehaviour
     string testingposturl = "http://127.0.0.1:5000/adduser/";
     string userDetails = "";
 
+    public TMP_InputField email;
+    public string emailText;
+    public TMP_InputField password;
+    public string passwordText;
+    public static bool userRole;
+
     public void getUser()
     {
         Debug.Log("Login button pressed");
@@ -27,23 +31,32 @@ public class LoginBehavior : MonoBehaviour
     }
 
     IEnumerator GetUserQuery(string email, string password)
-{
-    string url = "https://flask-mongo-backend-ar230500-famas.vercel.app/getuser/?email=" + email + "&password=" + password;
-    using (UnityWebRequest www = UnityWebRequest.Get(url))
     {
-        yield return www.SendWebRequest();
+        string url = "https://flask-mongo-backend-ar230500-famas.vercel.app/getuser/?email=" + email + "&password=" + password;
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
 
-        if (www.result == UnityWebRequest.Result.Success)
-        {
-            Debug.Log("Request successful!");
-            Debug.Log("Response: " + www.downloadHandler.text);
-        }
-        else
-        {
-            Debug.LogError("Request failed: " + www.error);
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Request successful!");
+                if (www.downloadHandler.text == "does not exist")
+                {
+                    Debug.Log("No user found");
+                }
+                else
+                {
+                    Debug.Log("Response: " + www.downloadHandler.text);
+                    SceneManager.LoadScene("MenuModified");
+                }
+                Debug.Log("Response: " + www.downloadHandler.text);
+            }
+            else
+            {
+                Debug.LogError("Request failed: " + www.error);
+            }
         }
     }
-}
 
     public void setText()
     {
@@ -57,78 +70,31 @@ public class LoginBehavior : MonoBehaviour
     }
 
     IEnumerator SetTheText(string id, string emailuser, string passworduser, string nameuser, string roleuser)
-{
-    // Create a JSON string manually
-    string jsonStr = "{\"_id\":\"" + id + "\",\"password\":\"" + passworduser + "\",\"name\":\"" + nameuser + "\",\"email\":\"" + emailuser + "\",\"role\":\"" + roleuser + "\"}";
-
-    // Create a UnityWebRequest instance
-    UnityWebRequest www = new UnityWebRequest(posturl, "POST");
-    byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonStr);
-    www.uploadHandler = new UploadHandlerRaw(jsonBytes);
-    www.SetRequestHeader("Content-Type", "application/json");
-
-    // Send the request and wait for the response
-    yield return www.SendWebRequest();
-
-    if (www.result == UnityWebRequest.Result.Success)
     {
-        Debug.Log("JSON data sent successfully!");
-        Debug.Log("Response: " + www.downloadHandler.text);
-    }
-    else
-    {
-        Debug.LogError("Failed to send JSON data: " + www.error);
-    }
+        // Create a JSON string manually
+        string jsonStr = "{\"_id\":\"" + id + "\",\"password\":\"" + passworduser + "\",\"name\":\"" + nameuser + "\",\"email\":\"" + emailuser + "\",\"role\":\"" + roleuser + "\"}";
 
-    // Clean up the UnityWebRequest
-    www.Dispose();
-}
-    public TMP_InputField email;
-    public string emailText;
-    public TMP_InputField password;
-    public string passwordText;
-    private MongoClient mongoClient;
-    private IMongoDatabase db;
-    public static bool userRole;
+        // Create a UnityWebRequest instance
+        UnityWebRequest www = new UnityWebRequest(posturl, "POST");
+        byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonStr);
+        www.uploadHandler = new UploadHandlerRaw(jsonBytes);
+        www.SetRequestHeader("Content-Type", "application/json");
 
-    private void connection()
-    {
-        string connectionString = "mongodb+srv://asadtariq1999:virtyou@testingvirtyou.ner4fbz.mongodb.net/?retryWrites=true&w=majority";
-        Debug.Log("Estabilishing connection...");
-        mongoClient = new MongoClient(connectionString);
-        Debug.Log("Connection Established...");
-        db = mongoClient.GetDatabase("Authentication");
-    }
+        // Send the request and wait for the response
+        yield return www.SendWebRequest();
 
-    private void authenticate(string email, string password)
-    {
-        connection();
-        var collection = db.GetCollection<BsonDocument>("users");
-        Debug.Log("Collection found");
-        var filter = Builders<BsonDocument>.Filter.Eq("email", email) &
-              Builders<BsonDocument>.Filter.Eq("password", password);
-        List<BsonDocument> results = collection.Find(filter).ToList();
-        // done authentication
-        
-
-        if (results.Count > 0)
+        if (www.result == UnityWebRequest.Result.Success)
         {
-            Debug.Log("Login Successful");
-            if (results[0]["role"].ToString() == "admin")
-            {
-                userRole = true;
-            }
-            else
-            {
-                userRole = false;
-            }
-            SceneManager.LoadScene("MenuModified");
+            Debug.Log("JSON data sent successfully!");
+            Debug.Log("Response: " + www.downloadHandler.text);
         }
         else
         {
-            Debug.Log("Login Failed");
+            Debug.LogError("Failed to send JSON data: " + www.error);
         }
-       
+
+        // Clean up the UnityWebRequest
+        www.Dispose();
     }
 
     public void Login()
@@ -142,7 +108,6 @@ public class LoginBehavior : MonoBehaviour
         //     SceneManager.LoadScene("MenuModified");
         // }
 
-        //getText();
         getUser();
         //setText();
 
