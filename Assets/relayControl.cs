023091411acc80588704740d7bab196be4273363
ar230500 <60130224+ar230500@users.noreply.github.com>
2023-05-06 15:03:04 +0100

    controller relay

--- Assets/relayControl.cs
+++ Assets/relayControl.cs
@@ -11,6 +11,11 @@ public class relayControl : MonoBehaviour
     private bool relayTwoOn = false;
     private string url = "";
 
+    void Update()
+    {
+        relayOutput();
+    }
+
     public void relayOutput()
     {
         StartCoroutine(sendRelayOutput());
@@ -18,30 +23,82 @@ public class relayControl : MonoBehaviour
 
     IEnumerator sendRelayOutput()
     {
-        if (LoginBehavior.userRole == "admin")
-        {
-            if (Input.GetButtonDown("B"))
+    //     if (LoginBehavior.userRole == "admin")
+    //     {
+    //         if (Input.GetButtonDown("B"))
+    //         {
+    //             Debug.Log("button B pressed");
+    //             if (relayOneOn == false)
+    //             {
+    //                 url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 1 + "&out_two=" + 0 + "&id=" + ObjectId.GenerateNewId();
+    //                 relayOneOn = true;
+    //             }
+    //             else
+    //             {
+    //                 url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 0 + "&out_two=" + 0 + "&id=" + ObjectId.GenerateNewId();
+    //             }
+    //         }
+    //         else if (Input.GetButtonDown("Y"))
+    //         {
+    //             if (relayTwoOn == false)
+    //             {
+    //                 url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 0 + "&out_two=" + 1 + "&id=" + ObjectId.GenerateNewId();
+    //                 relayTwoOn = true;
+    //             }
+    //             else
+    //             {
+    //                 url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 0 + "&out_two=" + 0 + "&id=" + ObjectId.GenerateNewId();
+    //             }
+    //         }
+
+    //         using (UnityWebRequest www = UnityWebRequest.Get(url))
+    //         {
+    //             yield return www.SendWebRequest();
+
+    //             if (www.result == UnityWebRequest.Result.Success)
+    //             {
+    //                 Debug.Log("Request successful!");
+    //                 if (www.downloadHandler.text == "Relay Outputs Configured")
+    //                 {
+    //                 Debug.Log("Response: " + www.downloadHandler.text);
+    //                 }
+    //                 else
+    //                 {
+    //                     Debug.Log("Error");
+    //                 }
+    //             }
+    //             else
+    //             {
+    //                 Debug.LogError("Request failed: " + www.error);
+    //             }
+    //         }
+
+    //         yield return new WaitForSeconds(5);
+    //     }
+        if (Input.GetButtonDown("B"))
             {
+                Debug.Log("button B pressed");
                 if (relayOneOn == false)
                 {
-                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 1 + "&out_two=" + 0 + "&id=" + ObjectId.GenerateNewId();
+                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/addrelayout/?out_one=1&out_two=0&id=" + ObjectId.GenerateNewId();
                     relayOneOn = true;
                 }
                 else
                 {
-                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 0 + "&out_two=" + 0 + "&id=" + ObjectId.GenerateNewId();
+                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/addrelayout/?out_one=" + "0" + "&out_two=" + "0" + "&id=" + ObjectId.GenerateNewId();
+                    relayOneOn = false;
                 }
             }
             else if (Input.GetButtonDown("Y"))
             {
                 if (relayTwoOn == false)
                 {
-                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 0 + "&out_two=" + 1 + "&id=" + ObjectId.GenerateNewId();
+                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/addrelayout/?out_one=" + "0" + "&out_two=" + "1" + "&id=" + ObjectId.GenerateNewId();
                     relayTwoOn = true;
                 }
                 else
                 {
-                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/adduser/?out_one=" + 0 + "&out_two=" + 0 + "&id=" + ObjectId.GenerateNewId();
+                    url = "https://flask-mongo-backend-ar230500-famas.vercel.app/addrelayout/?out_one=" + "0" + "&out_two=" + "0" + "&id=" + ObjectId.GenerateNewId();
                 }
             }
 
@@ -55,6 +112,7 @@ public class relayControl : MonoBehaviour
                     if (www.downloadHandler.text == "Relay Outputs Configured")
                     {
                     Debug.Log("Response: " + www.downloadHandler.text);
+                    
                     }
                     else
                     {
@@ -65,9 +123,9 @@ public class relayControl : MonoBehaviour
                 {
                     Debug.LogError("Request failed: " + www.error);
                 }
+                
             }
 
             yield return new WaitForSeconds(5);
-        }
     }
 }
